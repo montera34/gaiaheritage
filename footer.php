22f0b72ca2faf07ba80be24f0e7d77d36e949603
skotperez <voraginebramante@gmail.com>
2013-05-23 18:58:19 +0200

    footer and header styles

--- footer.php
+++ footer.php
@@ -1,11 +1,23 @@
+<?php
+// main page vars
+global $genvars;
+$genvars = array(
+	'blogname' => get_bloginfo('name'), // title
+	'blogdesc' => get_bloginfo( 'description', 'display' ), // description
+	'blogurl' => get_bloginfo('url'), // home url
+	'blogtheme' => get_bloginfo('template_directory'), // theme url
+);
+?>
 	</div><!-- .container -->
 </section>
 
 <footer>
 	<div id="epi" class="container">
 		<div class="row">
-			<div class="span4">
-			</div><!-- #gaia -->
+			<div class="span4 box-bordertop">
+				<?php echo $genvars['blogname']; ?><br />
+				<?php echo $genvars['blogdesc']; ?><br />
+			</div><!-- .box/bordertop -->
 		</div>
 	</div><!-- #pre -->
 </footer>
