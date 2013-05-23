714ab00a9fca4f0d5ae399e7244e1061542172f4
skotperez <voraginebramante@gmail.com>
2013-05-23 22:25:26 +0200

    filters in projects archive by form

---
+++ archive.php
@@ -0,0 +1,73 @@
+<?php
+get_header();
+?>
+
+<?php
+// if project
+if ( get_post_type( $post->ID ) == 'project' ) {
+	$args = array(
+		'post_type' => 'project',
+		'posts_per_page' => '-1',
+	);
+	$page_tit = "Projects";
+	$page_perma = get_permalink();
+	$loop = "mosac.img";
+	// filters
+	$years = get_terms( "year", $args );
+	$filters_out = "
+		<div class='span4'>
+			<form class='form-inline' name='selec-tax' action='" .$page_perma. "' method='get'>
+			<select name='year'>
+				<option value='' selected>All</option>
+	";
+	foreach ( $years as $term ) {
+		$filters_out .= "<option value='" .$term->slug. "'>" .$term->name. "</option>";
+	}
+	$filters_out .= "
+			</select>
+			<input type='submit' value='Filter' />
+			</form>
+		</div>
+	";
+}
+
+$the_query = new WP_Query( $args );
+if ( $the_query->have_posts() ) { ?>
+	<section>
+		<header>
+			<div class="row sec-space">
+				<div class="span4">
+					<h2 class="sec-tit"><?php echo $page_tit ?></h2>
+				</div>
+			</div>
+		</header>
+		<div class="row">
+			<div class="span4 box">
+
+				<div class="row mosac-row">
+				<?php echo $filters_out ?>
+				</div>
+				<div class="row mosac-row">
+	<?php // The Loop
+	$count = 0;
+	while ( $the_query->have_posts() ) : $the_query->the_post();
+		$count++;
+		include "loop.".$loop.".php";
+		if ( $count == 4 ) { $count = 0; }
+	endwhile;
+	/* Restore original Post Data 
+	 * NB: Because we are using new WP_Query we aren't stomping on the 
+	 * original $wp_query and it does not need to be reset.
+	*/
+	wp_reset_postdata(); ?>
+				</div><!-- .row -->
+			</div><!-- .box -->
+		</div><!-- row-->
+	</section>
+
+<?php } else {
+// if no posts in this loop
+
+} // end if have post ?>
+
+<?php get_footer(); ?>
