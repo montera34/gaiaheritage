f8ca06097435da46e43c3ed072ec5a65af159a86
skotperez <voraginebramante@gmail.com>
2013-05-24 13:10:59 +0200

    404 error page

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+get_header();
+?>
+	<section>
+		<header>
+			<div class="row sec-space">
+				<div class="span4">
+					<h2 class="sec-tit">Error 404: Content not found</h2>
+				</div>
+			</div>
+		</header>
+
+	</section>
+
+<?php get_footer(); ?>
