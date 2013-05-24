db1bbfe5147e5c9a19d301164377d54312b65b6a
skotperez <voraginebramante@gmail.com>
2013-05-24 10:55:36 +0200

    featured image in img mosac loop and some styles fixed

--- loop.mosac.img.php
+++ loop.mosac.img.php
@@ -5,13 +5,27 @@ $excerpt = get_the_excerpt();
 if ( $count == 1 ) { $item_class = " mosacimg-item-first"; }
 elseif ( $count == 4 ) { $item_class = " mosacimg-item-last"; }
 else { $item_class = ""; }
+
+if ( has_post_thumbnail() ) {
+	$img = get_the_post_thumbnail($post->ID,'thumbnail');
+	$img_out = "
+		<div class='mosacimg-item-img'>
+			".$img."
+		</div>
+		";
+} else {
+	$img_out = "";
+	$item_class .= " noimg";
+}
 ?>
 	<article>
-		<div class="span1 mosacimg-item<?php echo $item_class; ?>">
+		<div class="span1">
+			<div class="mosacimg-item<?php echo $item_class; ?>">
 			<a href="<?php echo $perma; ?>" title="<?php echo $tit; ?>">
 				<h3 class="boxitem-space mosacimg-item-tit"><?php echo $tit; ?></h3>
 				<div class="boxitem-space mosacimg-item-text"><?php echo $excerpt; ?></div>
 			</a>
-			<div class="mosacimg-item-img"><img src="wp-content/uploads/2013/01/brasilia-300x183.jpg" alt="" /></div>
+			<?php echo $img_out; ?>
+			</div>
 		</div>
 	</article>
