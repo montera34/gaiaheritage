e1cdf82e36564dd6e1eabc25165f5d4f4fff5902
skotperez <voraginebramante@gmail.com>
2013-06-17 11:59:56 +0200

    tagline width and padding relative

--- header.php
+++ header.php
@@ -76,7 +76,7 @@ wp_head(); ?>
 			<div id="gaia" class="span4">
 				<div class="row">
 					<h1 class="span1"><?php echo "<a href='" .$genvars['blogurl']. "' title='Ir al inicio'>" .$genvars['blogname']. "</a>"; ?></h1>
-					<div class="span1 offset2"><h2 style="padding-left: 175px;"><?php echo $genvars['blogdesc']; ?></h2></div>
+					<div class="span1 offset2"><h2 style="padding-left: 50%;"><?php echo $genvars['blogdesc']; ?></h2></div>
 				</div>
 			</div><!-- #gaia -->
 		</div>
