024e45f5023ba5ced1b687255aee44f64ec11506
skotperez <voraginebramante@gmail.com>
2013-10-23 16:37:13 +0200

    size and format values in downloads table

--- loop.table.php
+++ loop.table.php
@@ -1,43 +1,17 @@
 <?php
-		$tit = get_the_title();
-		$excerpt = get_the_excerpt();
-		$date_human = get_the_time('m\/d\/Y');
-		$date = get_the_time('Y-m-d');
-		$perma = get_post_meta( $post->ID, '_gaia_doc', true );
-		echo $perma;
-		echo "<br />";
-		//$perma_path = realpath($perma);	
-		//$perma_path = $_SERVER['DOCUMENT_ROOT']. "Gaia\\wp-content\\"realpath($perma);
-		//$perma_path = __FILE__;
-		echo basename($perma);
-		echo "<br />";
-		//$perma_path = realpath("./".basename($perma));	
-		$perma_path = $perma;	
-		echo $perma_path;
-		echo "<br />";
-		$size = round( filesize( $perma_path ) /1024 ) . " kB";
-		$format = mime_content_type($perma_path);
+$tit = get_the_title();
+$excerpt = get_the_excerpt();
+$date_human = get_the_time('m\/d\/Y');
+$date = get_the_time('Y-m-d');
+$perma = get_post_meta( $post->ID, '_gaia_doc', true );
+$path = parse_url($perma, PHP_URL_PATH);
+$doc_path = $_SERVER['DOCUMENT_ROOT'] . $path;
+$size = round( filesize( $doc_path ) /1024 ) . " kB";
+$format = mime_content_type($doc_path);
 $rel_project_url = get_post_meta( $post->ID, '_gaia_rel-project', true );
 $rel_project_tit = get_post_meta( $post->ID, '_gaia_rel-project-tit', true );
 if ( $rel_project_tit == '' || $rel_project_url == '' ) { $rel_project_out = "none"; }
 else { $rel_project_out = "<a href='" .$rel_project_url. "' title='" .$rel_project_tit. "'>" .$rel_project_tit. "</a>"; }
-//$args = array(
-//	'post_type' => 'attachment',
-//	'numberposts' => 1,
-//	'post_status' => null,
-//	'post_parent' => $post->ID,
-//);
-//$attachments = get_posts($args);
-//if ( $attachments ) {
-//	foreach ( $attachments as $attachment ) {
-//		$format = $attachment->post_mime_type;
-//		$size = round( filesize( get_attached_file( $attachment->ID ) ) /1024 ) . " kB";
-////echo get_attached_file( $attachment->ID);
-////echo getenv('DOCUMENT_ROOT');
-//print_r($_SERVER);
-////echo $_SERVER['DOCUMENT_ROOT'];
-//	} // end loop
-//} // end if attachment 
 ?>
 	<tr class="table-row">
 		<td><a href="<?php echo $perma; ?>" title="<?php echo $tit; ?>"><?php echo $tit; ?></a></td>
