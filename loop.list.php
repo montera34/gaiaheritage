1e697187fa3c1a72a35970a3397fe359b97f8d5d
skotperez <voraginebramante@gmail.com>
2014-08-20 15:37:25 +0200

    remove excerpt from loop list

--- loop.list.php
+++ loop.list.php
@@ -1,13 +1,14 @@
 <?php
 $tit = get_the_title();
 $perma = get_permalink();
-$excerpt = get_the_excerpt();
+if ( is_home() ) { $excerpt_out = ""; }
+else { $excerpt_out = "<div class='boxitem-space list-item-text'>" .get_the_excerpt(). "</div>"; }
 if ( $list_count == 1 ) { $list_class = "list-item-first"; }
 else { $list_class = "list-item"; }
 ?>
 	<article>
 		<div class="<?php echo $list_class ?>">
 			<h3 class="boxitem-space list-item-tit"><a href="<?php echo $perma; ?>" title="<?php echo $tit; ?>"><?php echo $tit; ?></a></h3>
-			<div class="boxitem-space list-item-text"><?php echo $excerpt; ?></div>
+			<?php echo $excerpt_out; ?>
 		</div>
 	</article>
