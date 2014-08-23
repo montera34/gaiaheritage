dffdec279b4b902393d0706ac978f840b65cbe52
skotperez <voraginebramante@gmail.com>
2014-08-23 11:56:30 +0200

    new template, inside default page template, for about and all pages with children

--- page.php
+++ page.php
@@ -3,41 +3,104 @@ get_header();
 ?>
 
 <?php
+if ( have_posts() ) { 
+while ( have_posts() ) : the_post();
+
+
 $page_tit = get_the_title();
-$loop = "mosac.text";
+// pages menu 
+$args = array(
+	'post_type' => 'page',
+	'sort_order' => 'ASC',
+	'sort_column' => 'menu_order',
+	'post_parent' => $post->ID,
+	'posts_per_page' => -1
+);
+$children = get_posts($args);
+//print_r($children);
+// if this page is parent and has children
+if ( $post->post_parent == '0' && count($children) >> 0 ) { 
+	$page_menu_out = "<header class='page-menu-item'><h1>" .$page_tit. "</h1></header>";
+	$menu_children_out = "";
+	foreach ( $children as $child ) {
+		$child_tit = $child->post_title;
+		$child_slug = $child->post_slug;
+		$child_perma = get_permalink($child->ID);
+		$page_menu_out .= "<div class='page-menu-item'><a href='" .$child_perma. "' title='" .$child_tit. "'>" .$child_tit. "</a></div>";
+	}
 
-if ( have_posts() ) { ?>
-	<section>
-		<header>
-			<div class="row sec-space">
-				<div class="span4">
-					<h2 class="sec-tit catcheye"><?php echo $page_tit ?></h2>
-				</div>
+// if this page is child
+} elseif ( $post->post_parent != '0' ) {
+	$parent_id = $post->post_parent;
+	$parent_tit = get_the_title($parent_id);
+	$parent_perma = get_permalink($parent_id);
+	$page_menu_out = "<div class='page-menu-item'><a href='" .$parent_perma. "' title='" .$parent_tit. "'>" .$parent_tit. "</a></div>";
+	$args = array(
+		'post_type' => 'page',
+		'sort_order' => 'ASC',
+		'sort_column' => 'menu_order',
+		'post_parent' => $post->post_parent,
+		'posts_per_page' => -1
+	);
+	$children = get_posts($args);
+	foreach ( $children as $child ) {
+		$child_tit = $child->post_title;
+		$child_slug = $child->post_slug;
+		$child_perma = get_permalink($child->ID);
+		if ( $child->ID == $post->ID ) {
+			$page_menu_out .= "<header class='page-menu-item'><h1>" .$page_tit. "</h1></header>";
+		} else {
+			$page_menu_out .= "<div class='page-menu-item'><a href='" .$child_perma. "' title='" .$child_tit. "'>" .$child_tit. "</a></div>";
+		}
+	}
+
+} // end if this page has children
+?>
+
+	<div class="row">
+		<section class="span3">
+			<div class="row">
+				<div class="span4 page-menu"><?php echo $page_menu_out ?></div>
 			</div>
-		</header>
-		<div class="row">
-			<div class="span4 box">
+			<article class="row">
+				<div class="span3 box">
+				<div class="boxitem-space">
+					<?php the_content(); ?>
+				</div><!-- .boxitem-space -->
+				</div><!-- .box -->
+			</article><!-- row-->
 
-				<div class="row mosac-row">
-	<?php // The Loop
-	$count = 0;
-	//while ( $the_query->have_posts() ) : $the_query->the_post();
-	while ( have_posts() ) : the_post();
-		$count++;
-		include "loop.".$loop.".php";
-		if ( $count == 4 ) { $count = 0; }
-	endwhile;
-	/* Restore original Post Data 
-	 * NB: Because we are using new WP_Query we aren't stomping on the 
-	 * original $wp_query and it does not need to be reset.
-	*/
-	wp_reset_postdata(); ?>
-				</div><!-- .row -->
-			</div><!-- .box -->
-		</div><!-- row-->
-	</section>
+		</section>
 
-<?php } else {
+		<section class="span1">
+
+<?php // links query
+$links = get_bookmarks();
+if ( $links[0] != '' ) {
+	echo "
+		<div class='row'>
+			<div class='span1'>
+				<header><h2 class='sec-tit'>Links</h2></header>
+				<div class='box'>
+	";
+	$which_loop = "links";
+	$list_count = 0;
+	foreach ( $links as $link ) {
+		$list_count++;
+		include "loop.list.php";
+	}
+	echo "
+				</div><!-- .box -->
+			</div><!-- .span1 -->
+		</div><!-- .row -->
+	";
+}
+?>
+		</section>
+	</div>
+<?php endwhile;
+} else {
+
 // if no posts in this loop
 
 } // end if have post ?>
