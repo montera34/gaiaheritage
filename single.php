7101ebe6101eb8268ff50c06d416694849b3eef1
skotperez <voraginebramante@gmail.com>
2014-09-11 19:26:11 +0200

    rel news section added to single new

--- single.php
+++ single.php
@@ -3,9 +3,29 @@ get_header();
 
 $page_tit = get_the_title();
 
-$loop = "single";
+// latest news loop
+$args = array(
+	'post_type' => 'new',
+	'posts_per_page' => 5,
+	
+);
+$relnews = get_posts($args);
+$relnews_out = "<div class='box'>";
+$rel_count = 0;
+foreach ( $relnews as $relnew ) {
+	$rel_count++;
+	if ( $rel_count == 1 ) { $relnew_class = "list-item-first"; }
+	else { $relnew_class = "list-item"; }
+	setup_postdata($relnew);
+	$relnew_tit = get_the_title( $relnew->ID );
+	$relnew_perma = get_permalink( $relnew->ID );
+	$relnews_out .= "<div class='" .$relnew_class. "'><h3 class='boxitem-space list-item-tit'><a href='" .$relnew_perma. "' title='" .$relnew_tit. "'>" .$relnew_tit. "</a></h3></div>";
+
+}
+$relnews_out .= "</div>";
+
 if ( have_posts() ) { ?>
-	<section>
+	<article <?php post_class(); ?>>
 		<header>
 			<div class="row sec-space">
 				<div class="span4">
@@ -14,25 +34,26 @@ if ( have_posts() ) { ?>
 			</div>
 		</header>
 		<div class="row">
-			<div class="span4 box">
+			<div class="span3 box">
 
-				<div class="row mosac-row">
+				<div class="boxitem-bigspace">
 	<?php // The Loop
 	$count = 0;
 	while ( have_posts() ) : the_post();
-		$count++;
-		include "loop.".$loop.".php";
-		if ( $count == 4 ) { $count = 0; }
-	endwhile;
-				echo $reldocs_out ?>
-				</div><!-- .row -->
+		the_content();
+	endwhile; ?>
+				</div><!-- .boxitem-bigspace -->
 			</div><!-- .box -->
+			<section id="relnews" class="span1">
+				<header><h2 class='sec-tit'>Latest news</h2></header>
+				<?php echo $relnews_out ?>
+			</section><!-- #relnews -->
 		</div><!-- row-->
-	</section>
+	</article>
 
 <?php } else {
 // if no posts in this loop
-	echo "No projects!";
+	echo "No news!";
 } // end if have post ?>
 
 <?php get_footer(); ?>
